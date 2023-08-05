d152074fe7c8e35139688a522b25f5549d221616
kuniaki <web.t.c.o.k@gmail.com>
2023-08-06 02:01:00 +0900

    users.jsのルーティング設定完了

--- sns/server.js
+++ sns/server.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const app = express();
+const userRoute = require("./routes/users");
 
 const PORT = 3000;
 
 app.listen(PORT, () => console.log("サーバーが起動しました"));
 
+//ミドルウェア
+app.use("/api/users", userRoute);
+
+
+
+
 app.get("/", (req, res) => {
     res.send("小沼です");
 });
+
+// app.get("/users", (req, res) => {
+//     res.send("users");
+// });
+
