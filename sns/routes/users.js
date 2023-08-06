10f8ccec2e53c9adb41bfcf2e647482318450aa8
kuniaki <web.t.c.o.k@gmail.com>
2023-08-06 14:46:59 +0900

    section3-20完了。ユーザー情報削除API実装。

--- sns/routes/users.js
+++ sns/routes/users.js
@@ -47,6 +47,26 @@ router.put("/:id", async(req, res) => {
 });
 
 
+//ユーザー情報の削除
+router.delete("/:id", async(req, res) => {
+    //req.params.id　→　リクエストで送られてきた/:idの情報
+    //上記IDが一致、またはisAdimnがtrueならログインしている（本人）と証明されるため変更可能とする
+    if(req.body.userId  === req.params.id || req.body.isAdmin){
+        try {
+            //上記if条件に合っていれば、//User（mongoose.Schemaで作ったインスタンス）のfindByIdAndUpdate関数を使用して
+            //ユーザ情報を見つけて更新する処理を実行する
+            //第一引数どのユーザー情報を更新するか指定（で入力されたユーザーIDを指定）、第二引数にオブジェクトで更新内容を記述
+            const user = await User.findByIdAndDelete(req.params.id);
+            res.status(200).json("ユーザ情報が削除されました");
+             
+        } catch(err) {
+            return res.status(500).json(err);
+        }
+
+    } else {
+        return res.status(403).json("自分のアカウントの時のみ情報を削除できます");
+    }
+});
 
 
 //こちらのファイルをエクスポート
