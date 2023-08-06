3e3620cfb108628fb21b3fa6aeaaddf2af7045b5
kuniaki <web.t.c.o.k@gmail.com>
2023-08-06 14:27:05 +0900

    section3-19完了。ユーザー情報の更新API実装。

--- sns/routes/users.js
+++ sns/routes/users.js
@@ -1,14 +1,51 @@
 //expressのルーティング関数を読み込み
 const router = require("express").Router();
 
+//UserSchemaの読み込み
+const User = require("../models/User");
+
+
 
 //ルーティング関数を使用してルーティング設定（server.jsで設定したエンドポイント=こちらのファイルではルートとなる）
-router.get("/", (req, res) => {
-    res.send("posts router");
+// router.get("/", (req, res) => {
+//     res.send("posts router");
+// });
+// router.get("/profile", (req, res) => {
+//     res.send("posts profile");
+// });
+
+
+//CRUD操作
+//ユーザー情報の更新
+//ユーザー情報の削除
+//ユーザー情報の取得
+//ユーザー情報の登録　→ auth.jsで実装
+
+//ユーザー情報の更新
+router.put("/:id", async(req, res) => {
+    //req.params.id　→　リクエストで送られてきた/:idの情報
+    //上記IDが一致、またはisAdimnがtrueならログインしている（本人）と証明されるため変更可能とする
+    if(req.body.userId  === req.params.id || req.body.isAdmin){
+        try {
+            //上記if条件に合っていれば、//User（mongoose.Schemaで作ったインスタンス）のfindByIdAndUpdate関数を使用して
+            //ユーザ情報を見つけて更新する処理を実行する
+            //第一引数どのユーザー情報を更新するか指定（で入力されたユーザーIDを指定）、第二引数にオブジェクトで更新内容を記述
+            const user = await User.findByIdAndUpdate(req.params.id, {
+                //$serは全てのパラメーターという意味　→ userデータスキーマで記述したパラメータ全てという意味.
+                //上記全てをリクエストで渡ってきた(現段階ではPOATMANで打ち込んだbody内容)情報に書き換える（req.body）
+                $set: req.body,
+            });
+            res.status(200).json("ユーザ情報が更新されました");
+             
+        } catch(err) {
+            return res.status(500).json(err);
+        }
+
+    } else {
+        return res.status(403).json("自分のアカウントの時のみ情報を更新できます");
+    }
 });
-router.get("/profile", (req, res) => {
-    res.send("posts profile");
-});
+
 
 
 
