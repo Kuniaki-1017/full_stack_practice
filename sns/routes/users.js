4ab2d8e8e82e115b680e1eb39aed9c3ee27db284
kuniaki <web.t.c.o.k@gmail.com>
2023-08-06 15:34:59 +0900

    section3-21完了。ユーザー情報の取得API実装。user._doc → 取得したuser情報の全てのプロパティを取得し、分割代入とスプレット構文で（{password, updatedAt, ...other} = user._doc;）不必要な情報を除いたオブジェクトを生成し返す処理を実装。

--- sns/routes/users.js
+++ sns/routes/users.js
@@ -23,7 +23,7 @@ const User = require("../models/User");
 
 //ユーザー情報の更新
 router.put("/:id", async(req, res) => {
-    //req.params.id　→　リクエストで送られてきた/:idの情報
+    //req.params.id　→　リクエストパラメーターで送られてきた/:idの情報
     //上記IDが一致、またはisAdimnがtrueならログインしている（本人）と証明されるため変更可能とする
     if(req.body.userId  === req.params.id || req.body.isAdmin){
         try {
@@ -49,13 +49,13 @@ router.put("/:id", async(req, res) => {
 
 //ユーザー情報の削除
 router.delete("/:id", async(req, res) => {
-    //req.params.id　→　リクエストで送られてきた/:idの情報
+    //req.params.id　→　リクエストパラメーターで送られてきた/:idの情報
     //上記IDが一致、またはisAdimnがtrueならログインしている（本人）と証明されるため変更可能とする
     if(req.body.userId  === req.params.id || req.body.isAdmin){
         try {
-            //上記if条件に合っていれば、//User（mongoose.Schemaで作ったインスタンス）のfindByIdAndUpdate関数を使用して
-            //ユーザ情報を見つけて更新する処理を実行する
-            //第一引数どのユーザー情報を更新するか指定（で入力されたユーザーIDを指定）、第二引数にオブジェクトで更新内容を記述
+            //上記if条件に合っていれば、//User（mongoose.Schemaで作ったインスタンス）のfindByIdAndDelete関数を使用して
+            //ユーザ情報を見つけて削除する処理を実行する
+            //どのユーザー情報を削除するか指定（で入力されたユーザーIDを指定）
             const user = await User.findByIdAndDelete(req.params.id);
             res.status(200).json("ユーザ情報が削除されました");
              
@@ -67,6 +67,25 @@ router.delete("/:id", async(req, res) => {
         return res.status(403).json("自分のアカウントの時のみ情報を削除できます");
     }
 });
+
+
+//ユーザー情報の取得
+router.get("/:id", async(req, res) => {
+    //req.params.id　→　リクエストパラメーターで送られてきた/:idの情報
+    //上記IDが一致、またはisAdimnがtrueならログインしている（本人）と証明されるため変更可能とする
+        try {
+            //上記if条件に合っていれば、//User（mongoose.Schemaで作ったインスタンス）のfindByIdById関数を使用して
+            //ユーザ情報を見つけて取得する処理を実行する
+            //どのユーザー情報を取得するか指定（で入力されたユーザーIDを指定）
+            const user = await User.findById(req.params.id);
+            res.status(200).json(user);
+             
+        } catch(err) {
+            return res.status(500).json(err);
+        }
+
+});
+
 
 
 //こちらのファイルをエクスポート
