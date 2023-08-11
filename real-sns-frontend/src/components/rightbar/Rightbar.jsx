9a38037f55c4fd874a0787ed273897bdb371e7dc
kuniaki <web.t.c.o.k@gmail.com>
2023-08-11 11:46:42 +0900

    section4-54完了。rightbar.jsx html完了

--- real-sns-frontend/src/components/rightbar/Rightbar.jsx
+++ real-sns-frontend/src/components/rightbar/Rightbar.jsx
@@ -2,5 +2,27 @@ import React from "react";
 import "./Rightbar.css";
 
 export default function Rightbar() {
-  return <div className="rightbar">Rightbar</div>;
+  return (
+    <div className="rightbar">
+      <div className="rightWrapper">
+        <div className="eventConteiner">
+          <img src="./assets/star.png" alt="" className="starImg" />
+          <span className="eventText">
+            <b>フォロワー限定</b>イベント開催中!
+          </span>
+        </div>
+        <img src="./assets/ad.jpeg" alt="" />
+        <h4 className="rightbarTitle">オンラインの友達</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgConteiner">
+              <img src="./assets/person/1.jpeg" alt="" className="" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">kuniaki</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
