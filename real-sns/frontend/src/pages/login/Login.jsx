3f71353dadeae3c69cd07c18bb20239be3f1c9f3
kuniaki <web.t.c.o.k@gmail.com>
2023-08-13 10:56:15 +0900

    section8-102完了。useRef関数を使用し監視、取得したいタグにref="任意"を記述。formタグのonSubmit属性を使用しbuttonがクリックされたときにrefで監視しているinputの要素、バリューを取得。

--- real-sns/frontend/src/pages/login/Login.jsx
+++ real-sns/frontend/src/pages/login/Login.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./Login.css";
 
 export default function Login() {
+  //要素を監視できる関数userRefを使用。監視したいタグに今回で言うとref="email"と記述。
+  const email = useRef();
+  const password = useRef();
+  console.log(email);
+
+  //form内のbuttonが押された時のイベントを定義
+  const hendleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email.current.value);
+    console.log(password.current.value);
+  };
   return (
     <div className="login">
       <div className="loginWrapper">
@@ -9,31 +20,32 @@ export default function Login() {
           <h3 className="loginLogo">Real SNS</h3>
           <span className="loginDesc">本格的なSNSを自分の手で</span>
         </div>
-        <div className="loginRight">
-          <form action="" className="loginForm">
-            <div className="loginBox">
-              <p className="loginMsg">ログインはこちら</p>
-              <input
-                type="email"
-                id="loginEmail"
-                className="loginInput"
-                placeholder="Eメール"
-                required
-              />
-              <input
-                type="password"
-                id="loginPassword"
-                className="loginInput"
-                placeholder="パスワード"
-                required
-                minLength="6"
-              />
-              <button className="loginButton">ログイン</button>
-              <span className="loginForgot">パスワードを忘れた方へ</span>
-              <button className="loginRegisterButton">アカウント作成</button>
-            </div>
-          </form>
-        </div>
+        {/* formタグにはonSubmit属性が使える　→ submitボタンを押した時の挙動を制御できる */}
+        <form className="loginRight" onSubmit={(e) => hendleSubmit(e)}>
+          <div className="loginBox">
+            <p className="loginMsg">ログインはこちら</p>
+            <input
+              type="email"
+              id="loginEmail"
+              className="loginInput"
+              placeholder="Eメール"
+              required
+              ref={email}
+            />
+            <input
+              type="password"
+              id="loginPassword"
+              className="loginInput"
+              placeholder="パスワード"
+              required
+              minLength="6"
+              ref={password}
+            />
+            <button className="loginButton">ログイン</button>
+            <span className="loginForgot">パスワードを忘れた方へ</span>
+            <button className="loginRegisterButton">アカウント作成</button>
+          </div>
+        </form>
       </div>
     </div>
   );
