8aec99078482f958d249252b490b5ef1bcc39f10
kuniaki <web.t.c.o.k@gmail.com>
2023-08-13 10:35:58 +0900

    section8-101完了。loginのバリデーション（htmlにて）完了

--- real-sns/frontend/src/pages/login/Login.jsx
+++ real-sns/frontend/src/pages/login/Login.jsx
@@ -10,18 +10,29 @@ export default function Login() {
           <span className="loginDesc">本格的なSNSを自分の手で</span>
         </div>
         <div className="loginRight">
-          <div className="loginBox">
-            <p className="loginMsg">ログインはこちら</p>
-            <input type="text" className="loginInput" placeholder="Eメール" />
-            <input
-              type="text"
-              className="loginInput"
-              placeholder="パスワード"
-            />
-            <button className="loginButton">ログイン</button>
-            <span className="loginForgot">パスワードを忘れた方へ</span>
-            <button className="loginRegisterButton">アカウント作成</button>
-          </div>
+          <form action="" className="loginForm">
+            <div className="loginBox">
+              <p className="loginMsg">ログインはこちら</p>
+              <input
+                type="email"
+                id="loginEmail"
+                className="loginInput"
+                placeholder="Eメール"
+                required
+              />
+              <input
+                type="password"
+                id="loginPassword"
+                className="loginInput"
+                placeholder="パスワード"
+                required
+                minLength="6"
+              />
+              <button className="loginButton">ログイン</button>
+              <span className="loginForgot">パスワードを忘れた方へ</span>
+              <button className="loginRegisterButton">アカウント作成</button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
