c55d0bf2f968a8ed2dd887dbd8664d7ad91fe0a6
kuniaki <web.t.c.o.k@gmail.com>
2023-08-13 13:57:11 +0900

    section8-108完了。actionCall.jsを作成。dispatch処理を作成

--- real-sns/frontend/src/state/AuthContext.js
+++ real-sns/frontend/src/state/AuthContext.js
@@ -9,18 +9,20 @@ const initialState = {
     isFetching: false,
     error: false,
 };
-
 //状態をグローバルに管理する
 //createContext関数でinitialStateをグローバルコンテキストとして定義
 export const AuthContext = createContext(initialState);
 
+
 //認証の状態をどこからでも提供できる関数を定義
 export const AuthContextProvider = ({children}) => {
 //useReducer関数を使用して状態管理を行う。第一引数に新しい状態にする関数、第二引数に初期値の状態を渡す
-//stateには今の状態が常に入り、dixpatchにはどういうactionを実行したか
+//stateには現在の状態が常に入り、dixpatchにはどういうactionを実行したか？するか？
     const [state, dispatch] = useReducer(AuthReducer, initialState);
+
+
     return (
-        // createContextを使用することで使えるタグ→どこにでも提供できる
+        // createContextを使用することでProvider修飾子を使えるタグ→どこにでも提供できる
         <AuthContext.Provider value={{
             user:state.user,
             isFetching: state.isFetching,
