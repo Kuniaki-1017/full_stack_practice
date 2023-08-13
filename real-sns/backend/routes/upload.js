ec468e98431a761b789927f348caf3efde18dbb6
kuniaki <web.t.c.o.k@gmail.com>
2023-08-13 22:43:14 +0900

    section8-126完了。

--- real-sns/backend/routes/upload.js
+++ real-sns/backend/routes/upload.js
@@ -1,8 +1,32 @@
 //expressのルーティング関数を読み込み
 const router = require("express").Router();
-
 //multerのインストール。ファイルのアップロードに使用される
 const multer = require("multer");
+
+
+// multerのdiskStorageを定義  
+const storage = multer.diskStorage({
+    destination: (req, file, cd) => {
+        cd(null, "public/images");
+    },
+    filename: (req, res, cd) => {
+        cd(null, file.originalname);
+    },
+});
+const upload = multer({storage});
+
+//画像アップロード用のAPI
+//第一引数：エンドポイント
+// 第二引数：ミドルウェアの設定（multerのupladeのsingle関数）
+// 第三引数:処理
+router.post("/",  upload.single("file"), (req, res) => {
+    try{
+        return res.status(200).json("画像アップロードに成功しました");
+
+    }catch(err) {
+        console.log(err);
+    }
+});
 
 
 module.exports = router;
